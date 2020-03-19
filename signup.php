54ce398e1cdb977ca987001712218bd74735aeed
matsuhashi-daigo <ts76cn@gmail.com>
2020-03-19 10:28:26 +0900

    no message

--- signup.php
+++ signup.php
@@ -115,9 +115,9 @@ require_once("lib.php");
 				//登録ユーザへ仮登録されたメールを送信
 				$mailTo = $mail;
 				$mailBcc = "ts76cn@gmail.com";
-				$body = <<< EOM
-				この度はご登録いただきありがとうございます。
-				本登録致しました。
+	$body = <<< EOM
+	この度はご登録いただきありがとうございます。
+	本登録致しました。
 EOM;
 				//Fromヘッダーを作成
 				$header = 'From: ' . mb_encode_mimeheader($companyname). ' <' . $companymail. '>';
